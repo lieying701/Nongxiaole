cfbd1b578226b2eab16f1b072cd387503caf331e
lieying701 <1505824535@>
2019-03-25 21:29:31 +0800

    test

---
+++ test/main/com/fea/entity/Person.java
@@ -0,0 +1,67 @@
+package com.fea.entity;
+
+import java.util.Date;
+
+/**
+ * @author fea701
+ * @description TODO
+ * @date 2019/3/25+21:23
+ **/
+public class Person {
+    private String name;
+    private int age;
+    private String sex;
+    private String address;
+    private Date birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
